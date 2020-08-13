c1dd75a3ba220186ef21b9612d38af777d8d9f88
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-08-12 23:08:30 -0600

    reconfigured SQL queries

--- queries.sql
+++ queries.sql
@@ -7,8 +7,8 @@ FROM Product as p
   ON p.CategoryID = c.ID;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-SELECT o.ID, o.ShipName as ShippingCo
-FROM Orders as o
+SELECT o.ID as orderID, o.ShipName as ShippingCo
+FROM [Order] as o
 WHERE o.orderDate < '2012-08-09'
 ORDER BY o.orderDate desc;
 
@@ -17,10 +17,10 @@ SELECT p.ProductName as Name, p.QuantityPerUnit as QuantityPer
 FROM Product as p
   JOIN OrderDetail as o 
 WHERE o.OrderID = 10251 AND o.ProductID = p.ID
-ORDER BY p.ProductName
+ORDER BY p.ProductName;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
-SELECT o.ID, c.CompanyName as CustomerCo, e.LastName as EmployeeLastName
-FROM Orders as o
-  JOIN Customer as c , Employee as e
-WHERE o.customerID = c.ID AND o.employeeID = e.ID
+SELECT o.ID as orderID, c.CompanyName as CustomerCo, e.LastName as EmployeeLastName
+FROM [Order] as o
+  JOIN Customer as c   , Employee as e
+WHERE o.customerID = c.ID AND o.employeeID = e.ID;
